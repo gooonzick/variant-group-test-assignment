211b78d233b884761ad17e560d8ce3e4ee714f04
George <tsamaladze@gmail.com>
2025-03-14 23:48:38 +0400

    feat: enhance styling and structure by adding reset.css, updating CSS variables, and introducing Textarea component

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -1,6 +1,8 @@
 import { Button } from "src/shared/ui/components/button";
 import type { Route } from "./+types/home";
 import { Input } from "src/shared/ui/components/input";
+import { Textarea } from "src/shared/ui/components/textarea";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,11 +12,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const [textareaValue, setTextareaValue] = useState("");
   return (
-    <div>
+    <div
+      style={{
+        padding: "20px",
+      }}
+    >
       <h1>Welcome to React Router!</h1>
       <Button variant="outline">Generate Now</Button>
-      <Input label="Job Title" />
+      <Button variant="primary">Generate Now</Button>
+      <Input label="Job Title" placeholder="Product manager" />
+      <Textarea
+        label="Additional details"
+        maxLength={1200}
+        value={textareaValue}
+        onChange={(e) => {
+          setTextareaValue(e.target.value);
+        }}
+      />
     </div>
   );
 }
---
+++ src/shared/ui/components/textarea/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState, type ComponentProps } from "react";
+
+import styles from "./textarea.module.css";
+
+type TextareaProps = ComponentProps<"textarea"> & {
+  label?: string;
+};
+
+export const Textarea: React.FC<TextareaProps> = ({
+  label,
+  rows = 9,
+  maxLength,
+  ...props
+}) => {
+  const [valueLength, setValueLength] = useState(
+    props.value ? String(props.value).length : 0
+  );
+
+  useEffect(() => {
+    if (maxLength) {
+      setValueLength(props.value ? String(props.value).length : 0);
+    }
+  }, [props.value, maxLength]);
+
+  const hasExceededMaxLength = !!maxLength && valueLength > maxLength;
+
+  return (
+    <label className={styles.container}>
+      {label && <span className={styles.label}>{label}</span>}
+      <textarea
+        {...props}
+        rows={rows}
+        className={styles.base}
+        aria-invalid={hasExceededMaxLength}
+      />
+      {!!maxLength && (
+        <span className={styles.limitLabel}>
+          {valueLength || 0}/{maxLength}
+        </span>
+      )}
+    </label>
+  );
+};
